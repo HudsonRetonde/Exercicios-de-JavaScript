330bdbf1576e8ee71b1db1e97440ef9ff2ed9a8e
Hudson Retonde <hudsonsretonde@gmail.com>
2022-07-18 21:10:54 -0300

    Subindo a função construtora correta, no commit anterior, subi novamente a factory.

--- Funções Construtoras/main.js
+++ Funções Construtoras/main.js
@@ -1,24 +1,17 @@
-function retornaHora(data){
-    if(data && !(data instanceof Date)){
-        throw new TypeError('Esperando instância de Date');
-    }
-    if(!data){
-        data = new Date();
-    }
-    return data.toLocaleTimeString('pt-BR',{
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hour12: false
-    });
+function Pessoa (nome, sobrenome) {
+    const ID = 123456;
+
+    const metodoInterno = function() {
+
+    };
+
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+
+    this.metodo = function() {
+        console.log(this.nome + ': sou um método!')
+    };
 }
 
-try{
-    const data = new Date('01-01-1970 12:58:12');
-    const hora = retornaHora(11); //se deixar vazio, o código dá certo, se deixar algo dá o erro.
-    console.log(hora);
-} catch(e) {
-    // Tratar erro.
-}finally{
-    console.log('Tenha um ótimo dia!');
-}
+const p1 = new Pessoa('Rock', 'Balboa');
+const p2 = new Pessoa('Daniel', 'Larusso');
