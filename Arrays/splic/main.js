2cc0e5d5755cfce6ee9e8f58af6c516b27feb5bd
Hudson Retonde <hudsonsretonde@gmail.com>
2022-07-22 10:14:09 -0300

    início ao array.

---
+++ Arrays/splic/main.js
@@ -0,0 +1,10 @@
+//               -6        -5      -4          -3          -2           -1
+//                0         1       2           3           4            5
+const nomes = ['Pedro', 'Tiago', 'João', 'no barquinho', 'no mar', 'da Galiléia.']
+ // nomes.splice(índice, delete, elem1, elem2, elem3 ....)
+ // pop
+
+ const removidos = nomes.splice(3, 2);
+ console.log(nomes, removidos);
+
+  
---
+++ Arrays/split/main.js
@@ -0,0 +1,13 @@
+/* const nome = ("Hudson dos Santos Retonde");
+
+const nomes = nome.split(' ');
+
+console.log(nomes); */
+
+const nomes = ['Hudson', 'dos Santos', 'Retonde'];
+const nome = nomes.join(' ');
+console.log(nome);
+
+
+
+  
