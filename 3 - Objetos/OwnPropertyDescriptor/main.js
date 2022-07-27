faf1c9d853afa8d8238673c97a243d31fa5466d1
Hudson Retonde <hudsonsretonde@gmail.com>
2022-07-27 19:10:20 -0300

    OwnPropertyDescriptor.

---
+++ 3 - Objetos/OwnPropertyDescriptor/main.js
@@ -0,0 +1,7 @@
+const produto = { nome: 'Produto', preco: 1.8};
+Object.defineProperty(produto, 'nome', {
+    writable: false,
+    configurable: false
+})
+console.log(Object.getOwnPropertyDescriptor(produto, 'nome'));
+console.log(produto);
