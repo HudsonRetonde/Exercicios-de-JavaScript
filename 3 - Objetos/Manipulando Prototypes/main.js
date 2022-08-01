1aae314420d53307f49a176a8a7f4b54c65a1a6c
Hudson Retonde <hudsonsretonde@gmail.com>
2022-08-01 20:31:01 -0300

    Terminando 'Manipulando Prototypes' e Herança.

--- 3 - Objetos/Manipulando Prototypes/main.js
+++ 3 - Objetos/Manipulando Prototypes/main.js
@@ -1,7 +1,63 @@
-/*
-Inínicio da manipulação.
-*/
+/*//New Object  => Object.prototype
 
 const objA = {
     chaveA: 'A'
+    // __proto__: Object.prototype
 };
+
+const objB = {
+    chaveB: 'B'
+    // __proto__: objA
+};
+
+const objC = new Object();
+objC.chaveC = 'C';
+
+Object.setPrototypeOf(objB, objA);
+Object.setPrototypeOf(objC, objB);
+console.log(objB.chaveA);
+*/
+
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.desconto = function(percentual) {
+    this.preco = this.preco - (this.preco * (percentual /100));
+}
+
+Produto.prototype.aumento = function(percentual) {
+    this.preco = this.preco + (this.preco * (percentual /100))
+}
+
+const p1 = new Produto('Camiseta', 100);
+
+//Literal
+
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+};
+
+Object.setPrototypeOf(p2, Produto.prototype);
+
+p2.aumento(10);
+
+const p3 = Object.create(Produto.prototype, {
+    preco: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 42,
+    },
+    tamanho: {
+        writable: true,
+        configurable: true,
+        enumerable: true,
+        value: 55,
+    }
+});
+p3.aumento(10);
+console.log(p3);
+
